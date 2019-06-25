032309ca203433cd318112e11581ee8f74706efd
Elinex <eline.ax@gmail.com>
2019-06-24 19:56:38 -0700

    add mock data movies to get response

--- index.js
+++ index.js
@@ -2,10 +2,19 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const { movies, getMovies } = require('./movies.js');
+
 app.get('/', (req, res) => {
   res.send('Hello world!');
-})
+});
+
+// GET /api/movies
+app.get('/movies', (req, res) => {
+
+  res.send(getMovies(movies));
+  
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}.`);
-})
+});
---
+++ movies.js
@@ -0,0 +1,16 @@
+const movies = [
+  {title: 'Mean Girls', watched: true, year: 1995, runtime: '100 min', metascore: 46, imdbRating: '6.2'},
+  {title: 'Hackers', watched: true, year: 1995, runtime: '100 min', metascore: 46, imdbRating: '6.2'},
+  {title: 'The Grey', watched: true, year: 1995, runtime: '100 min', metascore: 46, imdbRating: '6.2'},
+  {title: 'Sunshine', watched: true, year: 1995, runtime: '100 min', metascore: 46, imdbRating: '6.2'},
+  {title: 'Ex Machina', watched: true, year: 1995, runtime: '100 min', metascore: 46, imdbRating: '6.2'},
+];
+
+const getMovies = (movies) => {
+  return movies;
+};
+
+module.exports = {
+  movies,
+  getMovies
+};
